9d0459c3e66951b1ff78d1b14f74b7e5d54429ba
Khondax <miguel.gonzalez.villa@gmail.com>
2025-11-07 18:58:24 +0000

    [TASK-3 & 4]: added filters by name, type and generation

--- app/components/pokemonList.tsx
+++ app/components/pokemonList.tsx
@@ -5,35 +5,21 @@ import PokemonCard from "./pokemonCard";
 import { pokemonServiceGetAll } from "../services/pokemonService";
 import { PokemonDetailsPaginated } from "../interfaces/pokemonInterface";
 import PokemonDetailsItem from "./pokemonDetails";
+import Select, { StylesConfig }  from 'react-select'
 
 export default function PokemonList({pokemonData, readFromFile}: {pokemonData: PokemonDetailsPaginated, readFromFile: boolean}) {
   const [pokemons, setPokemons] = useState(pokemonData.pokemonData);
   const [loading, setLoading] = useState(false); 
   const [activeModal, setActiveModal] = useState(null);
+  const [filter, setFilter] = useState('')
+  const [selectedTypes, setSelectedTypes] = useState<string[]>([]); // Filtro por tipos (múltiples)
+  const [typesList, setTypesList] = useState<any[]>([]); // Lista de tipos disponibles
+  const [selectedGeneration, setSelectedGeneration] = useState<any[]>([]); // Lista de tipos disponibles
+  const [generationsList, setGenerationsList] = useState<any[]>([]); // Lista de tipos disponibles
 
-  const handleOpenModal = (pokemonIndex) => {
-    setActiveModal(pokemonIndex)
-  }
-  const handleCloseModal = () => {
-    setActiveModal(null)
-  }
-
-  if (readFromFile) {
-    return (
-      <div>
-        {pokemons.map((pokemon, index) => (
-          <div key={index} className="pokemon-card">
-            <PokemonCard onClick={() => handleOpenModal(index)} pokemon={pokemon}/>
-            <PokemonDetailsItem isOpen={activeModal === index} pokemonDetailsItem={pokemon} onClose={handleCloseModal}/>
-            <p>--------</p>
-          </div>
-        ))}
-      </div>
-    );
-
-  } else {
-  
-    // Función para cargar todos los pokemones en paralelo
+  // ESTE MÉTODO ESTÁ AQUÍ COMO EJEMPLO, HABILITARLO MACHACARÍA LA POKEAPI
+  if (!readFromFile) {
+    // Función para cargar todos los pokemons en paralelo
     const loadAllPokemons = async () => {
       setLoading(true);
       let currentOffset = 100;
@@ -55,37 +41,87 @@ export default function PokemonList({pokemonData, readFromFile}: {pokemonData: P
     useEffect(() => {
       loadAllPokemons(); // Cargamos todos los pokemons en cuanto se carga la página
     }, []);
-  
-    return (
+  }
+
+  const handleOpenCard = (pokemonIndex) => {
+    setActiveModal(pokemonIndex)
+  }
+  const handleCloseCard = () => {
+    setActiveModal(null)
+  }
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value.toLowerCase());
+  }
+
+  const handleTypeChange = (selectedOptions) => {
+    const selected = Array.from(selectedOptions, option => option.value);
+    setSelectedTypes(selected); // Actualizamos el estado con los tipos seleccionados
+  };
+
+  const handleGenerationChange = (selectedOptions) => {
+    const selected = Array.from(selectedOptions, option => option.value);
+    setSelectedGeneration(selected); // Actualizamos el estado con los tipos seleccionados
+  };
+
+  const filteredPokemons = pokemons.filter((pokemon) =>  {
+    const matchesName = pokemon.evolutionChain.evolutions.some(element => element.name.toLowerCase().includes(filter))
+    const matchesType = selectedTypes.length === 0 || pokemon.types.some(type => selectedTypes.includes(type.toLowerCase()))
+    const matchesGeneration = selectedGeneration.length === 0 || selectedGeneration.includes(pokemon.generation)
+
+    return matchesName && matchesType && matchesGeneration
+  });
+
+  // Crear una lista de tipos de Pokémon únicos (sin repetidos)
+  useEffect(() => {
+    const allTypes = [...new Set(pokemons.map(pokemon => pokemon.types).flat())]
+    const typesOptions = allTypes.map(type => {
+      return {value: type, label: type, color: ''}
+    })
+
+    const allGenerations = [...new Set(pokemons.map(pokemon => pokemon.generation).flat())]
+    const generationsOptions = allGenerations.map(generation => {
+      return {value: Number(generation), label: String(generation), color: ''}
+    })
+
+    setTypesList(typesOptions); // Establece los tipos únicos disponibles
+    setGenerationsList(generationsOptions); // Establece los tipos únicos disponibles
+  }, [pokemons]);
+
+  return (
       <div>
-        {pokemons.map((pokemon, index) => (
-          <div key={index} className="pokemon-card">
-            <PokemonCard onClick={() => handleCardClick(index)} pokemon={pokemon}/>
-            <PokemonDetailsItem isOpen={activeModal === index} pokemonDetailsItem={pokemon} onClose={handleCloseModal}/>
+        <div className="box">
+          <input 
+            type="text" 
+            placeholder="Buscar por nombre..." 
+            value={filter} 
+            onChange={handleFilterChange}
+            className="searchbar"
+          />
+          <Select
+            closeMenuOnSelect={false}
+            isMulti
+            options={typesList}
+            onChange={handleTypeChange}
+            // styles={colourStyles}
+          />
+          <Select
+            closeMenuOnSelect={false}
+            isMulti
+            options={generationsList}
+            onChange={handleGenerationChange}
+            // styles={colourStyles}
+          />
+          {/* <button>Ordenar por ID</button> */}
+        </div>
+        {filteredPokemons.map((pokemon, index) => (
+          <div key={index} className="">
+            <PokemonCard onClick={() => handleOpenCard(index)} pokemon={pokemon}/>
+            <PokemonDetailsItem isOpen={activeModal === index} pokemonDetailsItem={pokemon} onClose={handleCloseCard}/>
             <p>--------</p>
           </div>
         ))}
       </div>
     );
-  }
 
 }
-
-// export default function PokemonList({ pokemonData }) {
-//   const pokemonList = use(pokemonData);
-
-//   return (
-//     <div>
-//       {pokemonList.map((pokemon, index) => (
-//         <div key={index} className="pokemon-card">
-//           <PokemonCard pokemon={pokemon}/>
-//            {/* <Link href={`/pokemon/${pokemon.name}`}>
-//               {`Acceder a los detalles de ${pokemon.name}`}
-//           </Link> */}
-//           <p>--------</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
